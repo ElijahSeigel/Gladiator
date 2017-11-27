b5b809ed197b5a7264cabccec03623b9b33e7823
Mcelroy <Connor.Mcelroy@kochind.com>
2017-11-27 10:45:21 -0600

    character nav fix

--- src/ENV.js
+++ src/ENV.js
@@ -3,17 +3,17 @@
 
 export default class ENV{
 	constructor(hght, wdth,level){
-		this.borders = [[{x: 0, y: hght-5}, {x:0, y:hght}, {x:wdth, y:hght-5}, {x:wdth, y:hght}]];		
+		this.borders = [[{x: 0, y: hght-5}, {x:0, y:hght}, {x:wdth, y:hght-5}, {x:wdth, y:hght}]];
 		this.render = this.render.bind(this);
 	}//end constructor
-	
+
 	render(ctx){
 		this.borders.forEach((boundry)=>{
 			ctx.save();
 			ctx.fillStyle = 'lime';
-			ctx.fillRect(0, 995, 2000, 5);
+			ctx.fillRect(0, 995, 1000, 5);
 			ctx.restore();
 		});
 	}
-	
+
 }//end environment
