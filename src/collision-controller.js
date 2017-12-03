5ee519ca550667886f6a000f9da8e7b1fbced55b
Mcelroy <Connor.Mcelroy@kochind.com>
2017-12-03 14:21:44 -0600

    collisions fixed!

--- src/collision-controller.js
+++ src/collision-controller.js
@@ -11,38 +11,21 @@ export default class CollisionController{
 		this.environment = env;
 	}
 
-	pointInRectangle(){
-
-	}
-
-	playerEnvironmentCollides(point){		
+	playerEnvironmentCollides(point){
+		//console.log(this.environment);
+		//console.log(point);
 		var collision = false;
 		//test
-		point = {x: 2, y: 4};
-		this.env = [[{x: 0, y: 0}, {x: 0, y: 10}, {x: 10, y: 10}, {x: 10, y: 0}]];
+		point = {x: 50, y: 5};
+		this.environment = [[{x: 0, y: 0}, {x: 0, y: 10}, {x: 10, y: 10}, {x: 10, y: 0}]];
 		//endtest
 		this.environment.forEach((polygon)=>{
-			/*if(this.pointInside(point, polygon)) {
+			if(this.pointInside(point, polygon)) {
 				collision = true;
-			}*/
-			//console.log(point);
-			//console.log(polygon);
-			//taken from https://math.stackexchange.com/questions/190111/how-to-check-if-a-point-is-inside-a-rectangle
-			collision = !(this.areaRectangle(polygon[0], polygon[1], polygon[3]) < this.areaTriangle(point, polygon[0], polygon[1]) + this.areaTriangle(point, polygon[1], polygon[2]) + this.areaTriangle(point, polygon[2], polygon[3]) + this.areaTriangle(point, polygon[3], polygon[0]));
-
+			}
 		});
 		//console.log(collision);
 		return collision;
-
-	}
-
-	areaTriangle(pointA, pointB, pointC){
-		//taken from https://www.mathopenref.com/coordtrianglearea.html
-		return Math.abs((pointA.x*(pointB.y-pointC.y)+pointB.x*(pointC.y-pointA.y)+pointC.x*(pointA.y-pointB.y))/2);
-	}
-
-	areaRectangle(pointA, pointB, pointC){
-		return Math.abs(Math.sqrt(Math.pow((pointB.x-pointA.x),2)+Math.pow((pointB.y-pointA.y),2))*Math.sqrt(Math.pow((pointC.x-pointA.x),2)+Math.pow((pointC.y-pointA.y),2)));
 	}
 
   //0 -> colinear, 1 -> clockwise, 2 -> counterclockwise
@@ -87,16 +70,17 @@ export default class CollisionController{
 			console.log('pointInside must receive a polygon (at least 3 points)!');
 			return false;
 		}
-		var pointExtended = {x: point.x + 5000, y: point.y};
+		var pointExtended = {x: point.x + 1000, y: point.y};
 		var numIntersections = 0;
 
-		for(var i = 0; i < numPoints-1; i++){
-			if(this.doIntersect(polygonPoints[i], polygonPoints[i+1], point, pointExtended)){
-				console.log('intersection');
-				if(this.getOrientation(polygonPoints[i], point, polygonPoints[i+1]) == 0){
+		for(var i = 0; i < numPoints; i++){
+			var next = (i+1)%numPoints;
+			if(this.doIntersect(polygonPoints[i], polygonPoints[next], point, pointExtended)){
+				if(this.getOrientation(polygonPoints[i], point, polygonPoints[next]) == 0){
 					//console.log("oriented");
-					return this.onSegment(point, polygonPoints[i], polygonPoints[i+1]);
+					return this.onSegment(point, polygonPoints[i], polygonPoints[next]);
 				}
+				console.log('intersection');
 				numIntersections++;
 			}
 		}
