afc9f4df97f158bbd16519c12ce76bcea48370a2
Travis <travis14@ksu.edu>
2017-12-04 11:06:13 -0600

    12/4 started on basic level design

--- src/ENV.js
+++ src/ENV.js
@@ -6,24 +6,31 @@ export default class ENV{
 	constructor(hght, wdth,level){
 		this.height = hght;
 		this.width = wdth;
-		this.borders = [[{x: 0, y: hght}, {x: wdth, y:hght}, {x: wdth, y: hght-50}, {x: 0, y: hght-50}],
-										[{x: 0, y: hght-60}, {x: 0, y: hght - 100}, {x: wdth/2, y: hght-100}, {x:wdth/2, y: hght-60}]];
+this.borders = [[{x: 0, y: hght-50}, {x: 0, y: hght}, {x: wdth, y: hght}, {x: wdth, y: hght-50}],
+							[{x: 0, y: 450},{x: 0, y: 500},{x: 1300, y: 500},{x: 1300, y: 450}],
+							[{x: 200, y: 300},{x: 200, y: 350},{x: 1500, y: 350},{x: 1500, y: 300}]];
 		this.render = this.render.bind(this);
 		this.objectController = new ObjectController();
 		this.objectController.newObject('potion', 100, 100);
 	}//end constructor
 
 	render(ctx){
+		ctx.save();
+		ctx.fillStyle = 'lime';
 		this.borders.forEach((boundry)=>{
-			ctx.save();
-			ctx.fillStyle = 'lime';
-			ctx.fillRect(0, this.height, this.width, -50);
-			ctx.fillRect(0, this.height-60, this.width/2, -40);
-			ctx.restore();
-
-			this.objectController.render(ctx);
+			ctx.beginPath();
+			var point = 1;
+			ctx.moveTo(boundry[0].x, boundry[0].y);
+			while(point < boundry.length){
+				ctx.lineTo(boundry[point].x, boundry[point].y);
+				point++;
+			}
+			ctx.lineTo(boundry[0].x, boundry[0].y);
+			ctx.fill();
+			ctx.closePath();
+			//ctx.fillRect(50, 600, 1400, 50);
 		});
-
+		ctx.restore();
 	}
 
 }//end environment
